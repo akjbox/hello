a53906f3c21cb853b1d65bdbfb464cef2c0bf2ad
akjbox <akjbox@gmail.com>
2017-03-15 10:42:08 +0100

    new file
    new file

---
+++ test.java
@@ -0,0 +1,16 @@
+import java.util.Arrays;
+
+public class FirstDayAtSchool {
+
+	public String[] prepareMyBag() {
+		String[] schoolbag = {"Books", "Notebookss", "Penss"};
+		System.out.println("My school bag contains: "+Arrays.toString(schoolbag));
+		return schoolbag;
+	}
+
+	public String[] addPencils() {
+		String[] schoolbag = {"Books", "Notebookss", "Penss", "Pencilss"};
+		System.out.println("Now my school bag contain: "+Arrays.toString(schoolbag));
+		return schoolbag;
+	}
+}
